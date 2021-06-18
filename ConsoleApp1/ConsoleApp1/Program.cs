81a1794b4108854826a9ba74a99617421429a14b
Samir Flores Miranda <samirfmiranda@gmail.com>
2021-06-18 16:43:43 -0400

    Primer commit, ejercicio fibonacci

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = Fibonacci(10);
+            Console.WriteLine(result);
+            Console.ReadKey(true);
+        }
+        static string Fibonacci(int n)
+        {
+            string result = "";
+            int n1 = 0;
+            int n2 = 1;
+            int sum;
+
+            for (int i = 0; i < n; i++)
+            {
+                result += $"{ n1 } ";
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+                
+            }
+
+            return result;
+        }
+    }
+}
