2be8e9065bdef81034840d0464aadf5a46ad5e79
Samir Flores Miranda <samirfmiranda@gmail.com>
2021-06-21 16:20:10 -0400

    Ejercicio TiposDeDatos

---
+++ tiposDatos/TiposDatos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TiposDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Values
+            int a = -100;
+            Console.WriteLine(a);
+            int b = -128;
+            Console.WriteLine(b);
+            int c = 3540;
+            Console.WriteLine(c);
+            int d = 64876;
+            Console.WriteLine(d);
+            uint e = 2147483648;
+            Console.WriteLine(e);
+            int f = -1141583228;
+            Console.WriteLine(f);
+            long g = -1223372036854770;
+            Console.WriteLine(g);
+            int h = 808;
+            Console.WriteLine(h);
+            int i = 2_000_000;
+            Console.WriteLine(i);
+            int j = 0b_0001_1110_1000_0100_1000_0000;
+            Console.WriteLine(j);
+            int k = 0x_001E_8480;
+            Console.WriteLine(k);
+
+            decimal l = 3.141592653589793238m;
+            Console.WriteLine(l);
+            double m = 1.60217657;
+            Console.WriteLine(m);
+            decimal n = 7.8184261974584555216535342341m;
+            Console.WriteLine(n);
+        }
+    }
+}
