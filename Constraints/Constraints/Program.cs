0c30777584670ff386b98e32faca3d18fe0b315a
Samir Flores Miranda <samirfmiranda@gmail.com>
2021-06-29 10:31:14 -0400

    Ejercicio Constraints

---
+++ Constraints/Constraints/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Constraints
+{
+    public class UniqueInstance<T> where T: new()
+    {
+        public UniqueInstance(){ }
+
+        //public static T Unique = new();
+        public static T Unique { get; } =( Unique == null ? new(): Unique);
+    }
+    public class Person : UniqueInstance<Person>
+    {
+        public string Name;
+        public string LastName;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.Unique.Name = "pepe";
+            Person.Unique.LastName = "suarez";
+            Console.WriteLine(Person.Unique.Name);
+        }
+    }
+}
