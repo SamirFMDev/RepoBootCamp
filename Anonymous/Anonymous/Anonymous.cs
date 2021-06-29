b47e7c86333562ced99d7e9bf928b76ea1722c87
Samir Flores Miranda <samirfmiranda@gmail.com>
2021-06-29 17:42:41 -0400

    Ejercicio Anonymous Types

---
+++ Anonymous/Anonymous/Anonymous.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous
+{
+    class Anonymous
+    {
+        public object getData()
+        {
+            return new { Name = "Pepe", EmailID = "pepe@gmail.com" };
+        }
+    }
+}
---
+++ Anonymous/Anonymous/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Anonymous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Anonymous anonymous = new Anonymous();
+            dynamic anonymousDynamicData = anonymous.getData();
+            Console.WriteLine(string.Format("{0} {1}", anonymousDynamicData.Name, anonymousDynamicData.EmailID));
+
+            object anonymousData = anonymous.getData();
+            var obj = toGeneric(anonymousData, new { Name = "", EmailID = "" }); 
+            Console.WriteLine(string.Format("{0} {1}", obj.Name, obj.EmailID));
+            
+        }
+        public static T toGeneric<T>(object obj, T type)
+        {
+            return (T)obj;
+        }
+    }
+}
