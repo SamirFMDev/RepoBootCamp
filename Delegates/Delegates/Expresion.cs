68f66f91da803460288e9da73865211e136ee116
Samir Flores Miranda <samirfmiranda@gmail.com>
2021-07-07 13:42:22 -0400

    Ejercicio Delegates

---
+++ Delegates/Delegates/Expresion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Expresion
+    {
+        public delegate int OperationDelegate();
+        private int first;
+        private int second;
+        public Expresion(int first, int second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+        private int Sum()
+        {
+            return first + second;
+        }
+        private int Substract()
+        {
+            return first - second;
+        }
+        private int Multiply()
+        {
+            return first * second;
+        }
+        public int ApllyOperator(Operation operation)
+        {
+            OperationDelegate operationDelegate;
+            switch (operation)
+            {
+                case Operation.Sum:
+                    operationDelegate = new OperationDelegate(Sum);
+                    return operationDelegate();
+                case Operation.Substract:
+                    operationDelegate = new OperationDelegate(Substract);
+                    return operationDelegate();
+                case Operation.Multiply:
+                    operationDelegate = new OperationDelegate(Multiply);
+                    return operationDelegate();
+                default:
+                    return -1;
+            }
+        }
+
+        public enum Operation{
+            Sum,
+            Substract,
+            Multiply
+        }
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expresion expresion = new(20, 10);
+            var result = expresion.ApllyOperator(Expresion.Operation.Sum);
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
